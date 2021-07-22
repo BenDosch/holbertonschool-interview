c278676f970afe6ad5dd5bf2f195b99367304242
Ben Dosch <benjamindosch@gmail.com>
2021-07-21 22:12:35 -0500

    ready to check task 0

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+""" You have n number of locked boxes in front of you. Each box is numbered
+sequentially from 0 to n - 1 and each box may contain keys to the other boxes.
+"""
+
+def canUnlockAll(boxes):
+    """Function that deterimins if all the lockboxes can be opened
+
+    Args:
+        boxes (list): List containing a list of ints (all positive)
+        represents boxes of keys.
+
+    Return:
+        True if all boxes can be opened, else False.
+    """
+    locked = boxes.copy()
+    opened = [0]
+    keys = []
+    keys.extend(locked[0])
+    
+    while True:
+        if len(keys) > 0:
+            key = keys[0]
+        else:
+            return False
+        if key not in opened and key < len(locked):
+            opened.append(key)
+            keys.extend(locked[key])
+        if len(opened) is len(locked):
+            return True
+        keys.pop(0)
+
+if __name__ is "__main__":
+    boxes = [[1], [2], [3], [4], []]
+    print(canUnlockAll(boxes))
+
+    boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+    print(canUnlockAll(boxes))
+
+    boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+    print(canUnlockAll(boxes))
---
+++ 0x00-lockboxes/README.md
@@ -0,0 +1,1 @@
+# Directory for lockbox questions
