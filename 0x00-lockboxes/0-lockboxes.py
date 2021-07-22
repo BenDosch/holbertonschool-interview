91eba53026d775b72420bc46d0c933f8fe215973
Ben Dosch <benjamindosch@gmail.com>
2021-07-22 14:56:52 -0500

    New optomized/simplified version

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -14,36 +14,35 @@ def canUnlockAll(boxes):
     Return:
         True if all boxes can be opened, else False.
     """
+
     locked = boxes.copy()
-    opened = [0]
-    keys = []
-    keys.extend(locked[0])
-
-    if len(locked) == 1:
-        return True
-
+    opened = []
+    keys = [0]
+    new_keys = []
     while True:
-        if len(keys) > 0:
-            key = keys[0]
-            if key in opened:
-                keys.remove(key)
-                continue
-        else:
+        for key in keys:
+            new_keys.extend(locked[key])  # Take keys out of box and add set asside
+            locked[key] = []
+            opened.append(key)  # Mark box opened
+
+        if len(opened) == len(locked):  # Check if all boxes have been opened
+                return True
+        if new_keys == []:  # check to see if there are any more keys left
             return False
-        if key not in opened and key < len(locked):
-            opened.append(key)
-            keys.extend(locked[key])
-        if len(opened) is len(locked):
-            return True
-        keys.pop(0)
+
+        keys = [x for x in (set(new_keys)) if x not in opened]  # get ready to check keys that were set asside.
+        new_keys = []
 
 
-"""if __name__ is "__main__":
+"""if __name__ == "__main__":
     boxes = [[1], [2], [3], [4], []]
-    print(canUnlockAll(boxes))
+    print(canUnlockAll(boxes))  # True
 
     boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-    print(canUnlockAll(boxes))
+    print(canUnlockAll(boxes))  # True
 
     boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-    print(canUnlockAll(boxes)) """
+    print(canUnlockAll(boxes))  # False
+
+    boxes  = [list(range(1000)) for x in range(1000)]
+    print(canUnlockAll(boxes))  # True """
