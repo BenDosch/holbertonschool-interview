2609017a03001e079e52d3c4c1535fe87dfd9755
Ben Dosch <benjamindosch@gmail.com>
2021-07-28 17:36:46 -0500

    ready to check

---
+++ 0x01-insert_in_sorted_linked_list/0-instert_number.c
@@ -0,0 +1,47 @@
+#include "lists.h"
+
+/**
+ * insert_node - inserts a node with a number into a
+ * sorted list with the end being the bigest number
+ * @head: head of list
+ * @number: value of node to insert
+ * Return: address of new node, Null on fail
+ */
+
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *current = *head, *next, *new;
+
+    if (*head == NULL)
+		return (NULL);
+	next = current->next;
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = number;
+	if (number < current->n)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+	while (1)
+	{
+		if (next->next == NULL)
+		{
+			next->next = new;
+			return (new);
+		}
+		else if (number <= next->n && number >= current->n)
+		{
+			current->next = new;
+			new->next = next;
+			return (new);
+		}
+		else
+		{
+			current = current->next;
+			next = next->next;
+		}
+	}
+}
