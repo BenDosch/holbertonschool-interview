1c748d475a35f78ff7d555403f73576a22232a2e
Ben Dosch <benjamindosch@gmail.com>
2021-07-29 18:07:42 -0500

    double derefrence

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -18,7 +18,7 @@ listint_t *insert_node(listint_t **head, int number)
 	if (new == NULL)
 		return (NULL);
 	new->n = number;
-	if (*head == NULL)
+	if (**head == NULL)
 		*head = new;
 		return new; 
 	if (number < current->n)
