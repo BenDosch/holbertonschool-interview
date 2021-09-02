c7deac6226e488d714f35029bb2023795b20e827
Ben Dosch <benjamindosch@gmail.com>
2021-09-02 12:50:32 -0500

    rearanged code and switched finally to keyboardinterupt exception

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -50,7 +50,6 @@ def main():
                 )
             else:
                 count += 1
-    finally:
         print("File size: {}".format(total_size))
         print(
             "200: {}\n401: {}\n403: {}\n404: {}\n405: {}\n500: {}"
@@ -60,15 +59,17 @@ def main():
                 status_codes["405"], status_codes["500"]
             )
         )
-    print("File size: {}".format(total_size))
-    print(
-        "200: {}\n401: {}\n403: {}\n404: {}\n405: {}\n500: {}"
-        .format(
-            status_codes["200"], status_codes["401"],
-            status_codes["403"], status_codes["404"],
-            status_codes["405"], status_codes["500"]
+        
+    except KeyboardInterrupt:
+        print("File size: {}".format(total_size))
+        print(
+            "200: {}\n401: {}\n403: {}\n404: {}\n405: {}\n500: {}"
+            .format(
+                status_codes["200"], status_codes["401"],
+                status_codes["403"], status_codes["404"],
+                status_codes["405"], status_codes["500"]
+            )
         )
-    )
 
 
 if __name__ is not "__main__":
