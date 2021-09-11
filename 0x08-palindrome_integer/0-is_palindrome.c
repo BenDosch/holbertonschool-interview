b21bb0dd6a50e1b2e5e1ada6ddb9207b8ae8d8fe
BenDoschGit <Benjamindosch@gmail.com>
2021-09-11 19:57:37 +0000

    ready for checker

--- 0x08-palindrome_integer/0-is_palindrome.c
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -1,12 +1,27 @@
-#include "palindrome.h">
+#include "palindrome.h"
 
 /**
  * is_palindrome: Function that checks whether or not a given unsigned integer
  *                is a palindrome.
  * @n: The number to be checked.
- * 
+ *
  * Returns: 1 if N is a palindrome, else 0.
  */
 
-int is_palindrome(unsigned long n){
+int is_palindrome(unsigned long n)
+{
+	unsigned long num = n, new = 0, diget;
+
+	while (num / 10 > 0)
+	{
+		diget = num % 10;
+		new = (10 * new) + diget;
+		num /= 10;
+	}
+	new = (10 * new) + num;
+
+	if (new == n)
+		return (1);
+
+	return (0);
 }
