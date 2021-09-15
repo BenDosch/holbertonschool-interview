624f480f253f4417896e47339c69e34a240ced30
Ben Dosch <benjamindosch@gmail.com>
2021-09-15 17:30:44 -0500

    directory setup

---
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -0,0 +1,12 @@
+#include "palindrome.h">
+
+/**
+ * is_palindrome: Function that checks whether or not a given unsigned integer
+ *                is a palindrome.
+ * @n: The number to be checked.
+ * 
+ * Returns: 1 if N is a palindrome, else 0.
+ */
+
+int is_palindrome(unsigned long n){
+}
