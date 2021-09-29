509466df001b51d2c1c38efbb0c73b5a10d250f6
Ben Dosch <benjamindosch@gmail.com>
2021-09-28 19:37:22 -0500

    ready for checker

---
+++ 0x0A-slide_line/0-slide_line.c
@@ -0,0 +1,102 @@
+#include "slide_line.h"
+
+/**
+ * slide_line: Function that slides and merges an array of integers.
+ * 
+ * @line: Pointer to an array of integers.
+ * @size: Number of elements in the array pointed to by line.
+ * @direction: Direction to slide and merge, either SLIDE_Left or SLIDE_RIGHT.
+ * 
+ * return: 1 on sucess or 0 on failure. 
+ */
+
+int slide_line(int *line, size_t size, int direction){
+
+    if (line){
+        if (direction == SLIDE_RIGHT){
+            return (slide_right(line, size));
+        }
+        else if (direction == SLIDE_LEFT){
+            return (slide_right(line, size));
+        }
+    }
+    return (0);
+}
+
+/**
+ * slide_right - Slides the array to the right.
+ *
+ * @line: Pointer to an array of integers.
+ * @size: Number of elements in the array pointed to by line.
+ * @direction: Direction to slide and merge, either SLIDE_Left or SLIDE_RIGHT.
+ * 
+ * returns: 1 on success.
+ */
+
+int slide_right(int *line, size_t size){
+    int i, j;
+    size_t final_index = size - 1;
+
+    for (i = final_index; i > 0; i--){
+        j = i - 1;
+        while (!line[j] && j >= 0)
+            j--;
+        if (j == -1)
+            break;
+        if (!line[i]){
+            line[i] = line[j];
+            line[j] = 0;
+            i++;
+        }
+        else if (line[i]){
+            if (line[i] == line[j]){
+                line[i] *= 2;
+                line[j] = 0;
+            }
+            else
+                line[i - 1] = line[j];
+        }
+        else
+            break;
+    }
+    return (1);
+}
+
+/**
+ * slide_left - Slides the array to the left.
+ *
+ * @line: Pointer to an array of integers.
+ * @size: Number of elements in the array pointed to by line.
+ * @direction: Direction to slide and merge, either SLIDE_Left or SLIDE_RIGHT.
+ * 
+ * returns: 1 on success.
+ */
+
+int slide_left(int *line, size_t size){
+    int i, j;
+    size_t final_index = size - 1;
+
+    for (i = 0; i < final_index; i++){
+        j = i + 1;
+        while (!line[j] && j <= final_index)
+            j++;
+        if (j > final_index)
+            break;
+        if (!line[i]){
+            line[i] = line[j];
+            line[j] = 0;
+            i--;
+        }
+        else if (line[i]){
+            if (line[i] == line[j]){
+                line[i] *= 2;
+                line[j] = 0;
+            }
+            else
+                line[i + 1] = line[j];
+        }
+        else
+            break;
+    }
+    return (1);
+}
---
+++ 0x0A-slide_line/README.md
@@ -0,0 +1,34 @@
+# 2048 (single line)
+
+## Requirements
+
+* All your files will be compiled on Ubuntu 14.04 LTS
+* Your programs and functions will be compiled with gcc 4.8.4 using the flags -Wall -Werror -Wextra and -pedantic
+* All your files should end with a new line
+* Your code should use the Betty style. It will be checked using betty-style.pl and betty-doc.pl
+* You are not allowed to use global variables
+* No more than 5 functions per file
+*  The prototypes of all your functions should be included in your header file called slide_line.h
+* All your header files should be include guarded
+
+## Tasks
+List of tasks with brief descriptions of each task.
+
+### [0. Slide line](https://github.com/BenDoschGit/holbertonschool-machine_learning/blob/main/supervised_learning/e/0-slide_line.c"0. Slide line")
+
+The goal of this task is to reproduce the [2048 game](https://play2048.co "2048 game") mechanics on a single horizontal line.
+
+Given an array of integers, we want to be able to slide & merge it to the left or to the right. Identical numbers, if they are contiguous or separated by zeros, should be merged (See example)
+
+* Write a function that slides and merges an array of integers
+* Prototype: int slide_line(int *line, size_t size, int direction);
+* Where line points to an array of integers containing size elements, that must be slided & merged to the direction represented by direction. direction can be either:
+	* SLIDE_LEFT
+	* SLIDE_RIGHT
+	* If it is something else, the function must fail
+	* Both macros SLIDE_LEFT and SLIDE_RIGHT must be defined in slide_line.h
+* Your function must return 1 upon success, or 0 upon failure
+* You are not allowed to allocate memory dynamically (malloc, calloc, …)
+
+---
+
