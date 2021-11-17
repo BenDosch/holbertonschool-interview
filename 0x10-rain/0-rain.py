6f784663d8bf3a01103f156060cfb6b55e164879
Ben Dosch <benjamindosch@gmail.com>
2021-11-17 12:55:17 -0600

    changed first line for checker

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """Module contains the function rain that given a list of non-negative integers
 representing the heights of walls with unit width 1, as if viewing the
 cross-section of a relief map, calculate how many square units of water will
