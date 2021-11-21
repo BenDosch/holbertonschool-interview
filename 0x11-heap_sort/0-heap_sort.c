9131c7136a2debcaa02c28a622fd546dc4a2fe89
Ben Dosch <benjamindosch@gmail.com>
2021-11-20 18:20:27 -0600

    ready for checker

---
+++ 0x11-heap_sort/0-heap_sort.c
@@ -0,0 +1,61 @@
+#include "sort.h"
+
+/**
+ * heapify - makes an array into a heap
+ * 
+ * @array: array to turn into a heap
+ * @size: size of array passed
+ * @root: parent node to swap with leaves if lesser
+ * @n: total size of array to print
+ */
+
+void heapify(int *array, size_t size, size_t root, size_t n)
+{
+	int temp;
+	size_t largest = root, left = (2 * root) + 1, right = (2 * root) + 2;
+
+	if (left < size && array[left] > array[largest])
+		largest = left;
+
+	if (right < size && array[right] > array[largest])
+		largest = right;
+
+	if (largest != root)
+	{
+		temp = array[root];
+		array[root] = array[largest];
+		array[largest] = temp;
+
+		print_array(array, n);
+
+		heapify(array, size, largest, n);
+	}
+}
+
+/**
+ * heap_sort - sorts an array of integers in ascending order using the
+ * sift-down Heap sort algorithm.
+ * 
+ * @array: array to sort
+ * @size: size of array
+ */
+
+void heap_sort(int *array, size_t size)
+{
+	int i, temp;
+
+	for (i = (size / 2) - 1; i >= 0; i--)
+		heapify(array, size, i, size);
+
+	for (i = (size - 1); i >= 0; i--)
+	{
+		temp = array[0];
+		array[0] = array[i];
+		array[i] = temp;
+
+		if (i != 0)
+			print_array(array, size);
+
+		heapify(array, i, 0, size);
+	}
+}
---
+++ 0x11-heap_sort/sort.h
@@ -0,0 +1,10 @@
+#ifndef SORT_H_
+#define SORT_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void heap_sort(int *array, size_t size);
+void heapify(int *array, size_t size, size_t root, size_t n)
+
+#endif
