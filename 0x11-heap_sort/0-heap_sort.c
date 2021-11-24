9962a45c048a9135e4d6a651433566b8584087c7
Ben Dosch <benjamindosch@gmail.com>
2021-11-24 15:32:10 -0600

    testing Dex's code to check that its not a checker issue

--- 0x11-heap_sort/0-heap_sort.c
+++ 0x11-heap_sort/0-heap_sort.c
@@ -1,63 +1,58 @@
 #include "sort.h"
 
 /**
- * heapify - makes an array into a heap
- * 
- * @array: array to turn into a heap
- * @size: size of array passed
- * @root: parent node to swap with leaves if lesser
- * @n: total size of array to print
+ * downsift - the downsift method for heap ort
+ * @a: the array being heapified
+ * @n: starting index for array
+ * @i: ending index for array
+ * @size: size for printing references
+ * Return: void no return
  */
 
-void heapify(int *array, size_t size, size_t root, size_t n)
+void downsift(int *a, int n, int i, size_t size)
 {
-	int temp;
-	size_t largest=root;
-	size_t left = (2 * root) + 1;
-	size_t right = (2 * root) + 2;
+	int m = i;
+	int l = 2 * i + 1;
+	int r = 2 * i + 2;
+	int t;
 
-	if (left < size && array[left] > array[largest])
-		largest = left;
+	if (l < n && a[l] > a[m])
+		m = l;
+	if (r < n && a[r] > a[m])
+		m = r;
 
-	if (right < size && array[right] > array[largest])
-		largest = right;
-
-	if (largest != root)
+	if (m != i)
 	{
-		temp = array[root];
-		array[root] = array[largest];
-		array[largest] = temp;
-
-		print_array(array, n);
-
-		heapify(array, size, largest, n);
+		t = a[i];
+		a[i] = a[m];
+		a[m] = t;
+		print_array(a, size);
+		downsift(a, n, m, size);
 	}
 }
 
 /**
- * heap_sort - sorts an array of integers in ascending order using the
- * sift-down Heap sort algorithm.
- * 
- * @array: array to sort
- * @size: size of array
+ * heap_sort - performs the heap sort on an array
+ * @array: The array being heap sorted
+ * @size: the size of the array
+ * Return: void, no return
  */
 
 void heap_sort(int *array, size_t size)
 {
-	int i, temp;
+	int i, t, n = size;
 
-	for (i = (size / 2) - 1; i >= 0; i--)
-		heapify(array, size, i, size);
-
-	for (i = (size - 1); i >= 0; i--)
+	if (!array || size < 2)
+		return;
+	for (i = (n - 2) / 2; i >= 0; i--)
+		downsift(array, n, i, size);
+	for (i = (n - 1); i >= 0; i--)
 	{
-		temp = array[0];
+		t = array[0];
 		array[0] = array[i];
-		array[i] = temp;
-
+		array[i] = t;
 		if (i != 0)
 			print_array(array, size);
-
-		heapify(array, i, 0, size);
+		downsift(array, i, 0, size);
 	}
 }
