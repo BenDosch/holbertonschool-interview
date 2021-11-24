42d2c49c1c594299452a87243e1a7c607c7f2c5c
Ben Dosch <benjamindosch@gmail.com>
2021-11-24 15:39:03 -0600

    added missing ;'s

--- 0x11-heap_sort/sort.h
+++ 0x11-heap_sort/sort.h
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 
 void heap_sort(int *array, size_t size);
-void heapify(int *array, size_t size, size_t root, size_t n)
-void print_array(const int *array, size_t size)
+void heapify(int *array, size_t size, size_t root, size_t n);
+void print_array(const int *array, size_t size);
 
 #endif
