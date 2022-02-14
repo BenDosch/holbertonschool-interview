1e2012beb992809220325f7d3a23c7b6014696ea
Ben Dosch <benjamindosch@gmail.com>
2022-02-14 14:30:07 -0600

    Ready for checker

---
+++ 0x1C-island_permeter/0-island_perimeter.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" Module containing a function
+that finds the perimiter of and island"""
+
+
+def island_perimeter(grid):
+    """Calculates the perimeter of an island"""
+    perimiter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                if i != 0 and grid[i - 1][j]:
+                    pass
+                else:
+                    perimiter += 1
+                if i != len(grid) - 1 and grid[i + 1][j]:
+                    pass
+                else:
+                    perimiter += 1
+                if j != 0 and grid[i][j - 1]:
+                    pass
+                else:
+                    perimiter += 1
+                if j != len(grid[i]) - 1 and grid[i][j + 1]:
+                    pass
+                else:
+                    perimiter += 1
+    return perimiter
+
+if __name__ == "__main__":
+    grid = [
+        [0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0]
+    ]
+    print(island_perimeter(grid))
---
+++ 0x1C-island_permeter/README.md
@@ -0,0 +1,29 @@
+# Island_Permeter
+
+1. [Tasks](#tasks)
+	1. [Island Perimeter](#0-island-perimeter)
+
+2. [Author](#author)
+
+## Tasks
+List of tasks with brief descriptions of each task.
+
+### [0. Island Perimeter](https://github.com/BenDoschGit/holbertonschool-interview/blob/main/0x1C-island_permeter/0-island_perimeter.py "0. Island Perimeter")
+
+Create a function that returns the perimeter of the island described in grid.
+
+* grid is a list of list of integers:
+* 0 represents water
+* 1 represents land
+* Each cell is square, with a side length of 1
+* Cells are connected horizontally/vertically (not diagonally).
+* grid is rectangular, with its width and height not exceeding 100
+* The grid is completely surrounded by water
+* There is only one island (or nothing).
+* The island doesn’t have “lakes” (water inside that isn’t connected to the water surrounding the island).
+
+---
+
+## Author
+
+[Benjamin Dosch](https://github.com/BenDoschGit)
